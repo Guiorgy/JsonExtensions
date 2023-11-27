66e1a5a1fb3cd9b68cd8a46c1abbe97d1476c5b6
Guiorgy <Guiorgy123@gmail.com>
2023-11-27 15:23:17 +0400

    use Array.Exists on arrays over IEnumerable.Any

---
+++ JsonMultiNameModifier/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Guiorgy.JsonExtensions
+{
+    internal static class Extensions
+    {
+        public static bool Any<TSource>(this TSource[] source, Predicate<TSource> predicate) => Array.Exists(source, predicate);
+    }
+}
